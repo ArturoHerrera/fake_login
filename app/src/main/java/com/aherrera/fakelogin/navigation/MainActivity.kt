ae9518c61abfde9ad0c3a96fc6cd7ceb3579f344
ArturoHerrera <joseherreravi92@gmail.com>
2024-07-25 21:54:23 -0600

    feat(structure) : Added initial packages structure.

--- app/src/main/java/com/aherrera/fakelogin/navigation/MainActivity.kt
+++ app/src/main/java/com/aherrera/fakelogin/navigation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.aherrera.fakelogin
+package com.aherrera.fakelogin.navigation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -20,28 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             FakeLoginTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FakeLoginTheme {
-        Greeting("Android")
     }
 }
